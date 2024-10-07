313291064a23fb6a296c72a2c5615d4453181a09
usr1mdr <john@macrodatarefinement.io>
2024-10-07 11:50:58 -0500

    Need to change what happens if table_in is not None. How does the inspector know which table to acacess when the method is called?

--- db.py
+++ db.py
@@ -1,41 +1,33 @@
 from sqlalchemy import create_engine, select
 from sqlalchemy.orm import declarative_base, sessionmaker
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, inspect
 # from dag import Node, Edge
 
 class database_query():
     def __init__(self):
         self.var = ''
-        # self.Base = declarative_base()
-        # self.engine = create_engine("sqlite://", echo=True)
-        # self.Base.metadata.create_all(self.engine)
-        # self.session = sessionmaker(self.engine)()
-
-    # def dag_func(self):
-        # node_table_name = 'node'
-        # node = Node()
-        # edge = Edge()
-        # n1 = node
-        # n2 = node
-        # e1 = edge(n1,n2)
-        # self.session.add_all([n1,n2,e1])
-        # self.session.commit()
-    
-    # def exec_sql(self, query):
-    #     with self.engine.connect() as conn:
-    #         for row in conn.execute(query):
-    #             print(row)
         
-    def select_func(self, table_in):
+    def select_func(self, table_in=None):
         __table_name__ = table_in
         Base = declarative_base()
-        engine = create_engine("sqlite://", echo=True)
-        Base.metadata.create_all(engine)
+        engine = create_engine("sqlite:///nodes.db", echo=True)
+        mymetadata = Base.metadata.create_all(engine)
         session = sessionmaker(engine)()
-        stmt = select(table_in)
-        print(stmt)
 
+        if table_in is None:
+            for item in mymetadata.sorted_tables:
+                print(item.c)
+                print(item.name)
+        # stmt = select(table_in)
+        # print(stmt)
 
+        # Inspecting the Schema
+        if table_in is not None:
+            inspector = inspect(engine)
+            columns = inspector.get_columns('node')
+            for column in columns:
+                print(f"Column: {column['name']}, Type: {column['type']}, Auto-Increment: {column['autoincrement']}")
+            self.session.close()
         # __table_name__ = 'node'
         # print(type(__table_name__))
         # node = Node(table_name)
@@ -48,9 +40,8 @@ class database_query():
         #     for row in conn.execute(stmt):
         #         print(row)
 
-        self.session.close()
-
-
+x = database_query()
+x.select_func()
         # engine = connect('sqlite:///links.db', echo=True)
     
     
